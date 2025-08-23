6aec475faff118f9ee22d1fb9e9ec8d98e508c60
R,H <162550688+RH0099@users.noreply.github.com>
2025-08-23 18:59:15 +0600

    Termux_Flipper.sh

---
+++ Termux_Flipper.sh
@@ -0,0 +1,14 @@
+pkg update && pkg upgrade
+pkg install python -y
+pkg install python-pip
+pip install pyusb
+pkg install libbluetooth-dev
+pip install pybluez
+pip install pyserial
+pkg install irutils
+pip install rich
+pkg install termux-api
+pkg instoll git
+https://github.com/RH0099/Termux-Flipper-zero.git
+cd Termux-Flipper-zero
+python Flipper.py
