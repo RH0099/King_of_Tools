bdd417d91edadbfa911ebe079ac387d47dfb1bbb
R,H <162550688+RH0099@users.noreply.github.com>
2025-08-23 16:50:04 +0600

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,34 @@
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install python-pip -y
+pkg install wget -y
+pkg install fish -y
+pkg install ruby -y
+pkg install git -y
+pkg install php -y
+pkg install perl -y
+pkg install parallel -y
+pkg install bash -y
+pkg install clang -y
+pkg install nano -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install sudo -y
+pkg install openssl -y
+pkg install toilet -y
+pkg install proot -y
+pkg install openssh 
+pkg install python-pip
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php
+termux-setup-storage
