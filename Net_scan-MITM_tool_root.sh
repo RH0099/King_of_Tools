3662adb5b5f1288167408045fa0a3e0f9f9d0e7f
R,H <162550688+RH0099@users.noreply.github.com>
2025-08-23 18:40:33 +0600

    Net_scan-MITM_tool_root.sh

---
+++ Net_scan-MITM_tool_root.sh
@@ -0,0 +1,24 @@
+
+pkg update && pkg upgrade -y
+
+pkg install python nmap git termux-api -y
+
+pip install mitmproxy
+
+pkg instoll python -y
+
+pkg instoll python3 -y
+
+pkg install dsniff -y 
+
+pkg install arp-scan -y 
+
+pkg instoll git -y
+
+pkg instoll nmap -y
+
+git clone https://github.com/RH0099/Natwark_scan-MITM_tools.git
+
+cd Natwark_scan-MITM_tools
+
+python Scanning_MITM.py
