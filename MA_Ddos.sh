098c86e2e33cf02739116dd86a8844cbac734ec5
R,H <162550688+RH0099@users.noreply.github.com>
2025-08-23 16:43:24 +0600

    M,A_Ddos.sh

---
+++ MA_Ddos.sh
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade 
+pkg instoll pip
+pkg install golang
+pkg instoll python 
+pip install -r requirements.txt
+pkg instoll git 
+git clone https://github.com/RH0099/M-A-Ddos.git
+cd M-A-Ddos
+python main.py
+RH
+RH
